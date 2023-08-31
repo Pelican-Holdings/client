c699626b1e8b78bd80cdd505f9d70288e4c9b7ca
YohanNayanajith <yohannayanajith40@gmail.com>
2023-08-31 11:10:15 +0530

    task/add header bar

---
+++ components/Header.tsx
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+    <div className="relative">
+      <header className="w-full  absolute z-10 bg-main invisible sm:visible">
+        <div className="max-w-[1440px] mx-auto sm:px-16 px-6 py-3 bg-transparent text-white">
+          Bitumen and
+          <span className="text-secondary font-bold"> Bituminous products</span>
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
---
+++ components/index.ts
@@ -0,0 +1,7 @@
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import Header from "./Header";
+
+export { Hero, CustomButton, Navbar, Footer, Header };
